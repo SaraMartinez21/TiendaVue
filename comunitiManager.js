7a216a1a6f18448576e85caccfa30b0b3a3e8e81
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2023-10-20 22:46:04 +0200

    casi terminado

--- comunitiManager.js
+++ comunitiManager.js
@@ -1,5 +1,4 @@
 export async function getJuegos(){
-    console.log('hola');
     const response = await fetch('./almacen.json');
     const juegos = await response.json();
     return juegos.videojuegos;
--- index.js
+++ index.js
@@ -10,37 +10,52 @@ createApp({
             juegos: [],
             juegoActual: [],
             
-        }
+        };
     },
 
     methods: {
-        getJson() {
-            getJuegos().then(nova => {
-                this.juegos = nova,
-                    console.log(this.juegos)
-            })
+        async getJson() {
+            this.juegos= await getJuegos();
+            console.log(this.juegos);
         },
 
         insertar(id){
 
-            const productoIncrementar = this.juegos.find(juego => juego.videojuegos === id);
+            const productoIncrementar = this.juegoActual.find(juego => juego.id === id);
 
             if(productoIncrementar){
                 productoIncrementar.cantidad+=1;
-                console.log(`Se ha incrementado la cantidad del producto ${id} en 1  unidades.`);
             }else{
                 this.juegoActual.push({id, cantidad: 1});
-                console.log(`No se encontró un producto con el ID ${id}`);
               
             }
             console.log(this.juegoActual);
         },
+        quitar(id){
+            const index = this.juegoActual.findIndex(juego => juego.id === id);
 
+            if(index !== -1){
+                if (this.juegoActual[index].cantidad > 1) {
+                    this.juegoActual[index].cantidad -= 1;  
+                }else{
+                    this.juegoActual.splice(index, 1);
+                }
+            }
+           
+        },
         mostrar(idDiv){
             return this.divActivo === idDiv;
         },
         cambiar(nuevoDiv){
             this.divActivo=nuevoDiv;
+        },
+        calcularTotal(){
+            let total = 0;
+            for (const actual of this.juegoActual) {
+                total += this.juegos[actual.id].precio * actual.cantidad;
+                
+            }
+            return total;
         }
     }
 
